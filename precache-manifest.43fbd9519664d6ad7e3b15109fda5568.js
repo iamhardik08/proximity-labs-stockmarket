2a9f426d225e8f269a930f36b2650cd02c10765d
DESKTOP-LTA4Q0V\asus <thardik40@gmail.com>
2020-08-03 10:54:33 +0530

    Updates

---
+++ precache-manifest.43fbd9519664d6ad7e3b15109fda5568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74bb9907681fa656baa435ee325a3b2",
+    "url": "/proximity-labs-stockmarket/index.html"
+  },
+  {
+    "revision": "5c172e9aa5ef93952d86",
+    "url": "/proximity-labs-stockmarket/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0863cac82df405ed898c",
+    "url": "/proximity-labs-stockmarket/static/js/2.545736a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proximity-labs-stockmarket/static/js/2.545736a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c172e9aa5ef93952d86",
+    "url": "/proximity-labs-stockmarket/static/js/main.c8d044e9.chunk.js"
+  },
+  {
+    "revision": "8cb6f08d39e677a459f7",
+    "url": "/proximity-labs-stockmarket/static/js/runtime-main.5974f072.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proximity-labs-stockmarket/static/media/logo.5d5d9eef.svg"
+  }
+]);
